aab583ae560841269d398990bc24d07e196ff84a
larabjork <lara.m.bjork@gmail.com>
2022-11-28 11:38:26 -0800

    fix errors in code from lesson 12.6

--- static/js/app.js
+++ static/js/app.js
@@ -12,26 +12,28 @@ function buildTable(data) {
         let row = tbody.append("tr");
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
-            cell.text(val):
+            cell.text(val);
         });
     });
 };
 
 function handleClick() {
-    // Grad datetime value from the filter
+    // Grab datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
 
     // Check to see if a date was entered and filter the data using that date
     if (date) {
         filteredData = filteredData.filter(row => row.datetime === date);
-    };
-
+    }
+    // Rebuild the table using the filtered data 
+    // !! If no date was entered, then filteredData will remain the original tableData!!
+    buildTable(filteredData);
 };
 
 // Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click, handleClick");
+d3.selectAll("#filter-btn").on("click", handleClick);
 
-// Rebuild the table using the filtered data 
-// !! If no date was entered, then filteredData will remain the original tableData!!
-buildTable(filteredData);
+// Build the table when the page loads
+buildTable(tableData);
+
